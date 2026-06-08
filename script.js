2905617654d2a12f5aad88aa270b8e2db17b0027
gowtham nalluri <nallurigowtham3338@gmail.com>
2026-06-09 01:54:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ history.scrollRestoration = "manual";
 window.addEventListener("load", () => {
     window.scrollTo(0, 0);
 });
+
+const navLinks = document.querySelectorAll('.nav-link');
+const menu = document.querySelector('.navbar-collapse');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('show');
+    });
+});
